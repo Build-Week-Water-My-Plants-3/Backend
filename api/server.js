723e753d1e5137c0b2adfa7ffbaa34fcc34ded0a
Laura <theimer.laura@gmail.com>
2020-02-26 22:28:26 -0500

    base server running on 5550, postman 200 ok

---
+++ api/server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
+
+// express routes 
+// const authRouter = require('../auth/authRouter.js');
+// const usersRouter = require('../users/usersRouter.js'); 
+
+
+// tokens 
+// const jwt = require('jsonwebtoken');
+
+const server = express();
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+// routes
+// server.use('/', authRouter);  // leads to /register /login /userhome  etc 
+
+// server.use('/users', usersRouter); 
+    // don't really need, but used to the set up.
+
+server.get('/testing', (req, res) => {
+    res.send(" Water My Plants 🌻");
+});  // testing base of localhost. before adding end points. 
+
+// // testing token 
+// server.get('/testing-token', (req, res) => {
+//     const payload = {
+//         subject: 'testtoken',
+//         userid: 'testytessa',
+//         favoritePlant: 'Rose'
+//     };
+
+//     const secret = "not your cup of tea";
+
+//     const options = {
+//         expiresIn: '1d'
+//     };
+
+//     const token = jwt.sign(payload, secret, options);
+
+//     res.json(token);
+//     });
+
+module.exports = server; 
+
+
---
+++ index.js
@@ -0,0 +1,5 @@
+const server = require('./api/server.js');
+
+const port = process.env.PORT || 5550;
+server.listen(port, () => 
+console.log(` ✨✨ Magic happening on port ${port}  ✨✨`)); 
